514d18aee8858c39929ebb966d42bdbf0135682e
Bruno <sautron.brunojj@gmail.com>
2015-06-15 16:13:03 +0200

    eriughserg

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -2,8 +2,35 @@
 
 using namespace	std;
 
-int		main(void)
+static char	*ft_strtoupper(char *str)
 {
-	cout << "eirohuf";
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
+int		main(int ac, char **av)
+{
+	int		i;
+
+	i = 1;
+	if (ac >= 2)
+	{
+		while (i < ac)
+		{
+			cout << ft_strtoupper(av[i]);
+			i++;
+		}
+	}
+	else
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	cout << endl;
 	return (0);
 }
