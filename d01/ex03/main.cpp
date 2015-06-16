50721010f8ddbb43e33bfae301b21b528b029c50
Bruno SAUTRON <bsautron@e3r8p8.42.fr>
2015-06-16 16:04:23 +0200

    seyurghserg

--- d01/ex03/main.cpp
+++ d01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/16 15:07:12 by bsautron          #+#    #+#             */
-/*   Updated: 2015/06/16 15:43:07 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/06/16 16:04:01 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 int		main(void)
 {
 	ZombieHorde		horde1 = ZombieHorde(10);
-
+	std::cout << "Horde 1" << std::endl;
 	horde1.announce();
+
+	ZombieHorde		horde2 = ZombieHorde(4);
+	std::cout << "Horde 2" << std::endl;
+	horde2.announce();
 	return (0);
 }
