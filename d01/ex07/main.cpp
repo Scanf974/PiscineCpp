ebe2de21178df85bb331442dbddd238ac807f851
Bruno SAUTRON <bsautron@e3r7p3.42.fr>
2015-06-17 18:44:00 +0200

    seriughserg

--- d01/ex07/main.cpp
+++ d01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/17 17:49:15 by bsautron          #+#    #+#             */
-/*   Updated: 2015/06/17 18:38:07 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/06/17 18:43:32 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 
 int		main(int ac, char **av)
 {
-	std::ifstream	ifs(av[1]);
-	std::string		str;
-	std::string		final_str = "";
-	std::string		s1, s2;
-	int				i = 0;
-	int				pos;
 
 	if (ac == 4)
 	{
+		std::ifstream	ifs(av[1]);
+		std::string		str;
+		std::string		final_str = "";
+		std::string		s1, s2;
+		std::string		s0 = av[1];
+		std::string		new_file = s0 + ".replace";
+		int				i = 0;
+		int				pos;
 		s1 = av[2];
 		s2 = av[3];
+
 		if (ifs) {
 			while ( getline(ifs, str) > 0) {
 				if (i != 0)
@@ -36,7 +39,7 @@ int		main(int ac, char **av)
 			while ((pos = final_str.find(s1)) != -1)
 				final_str.replace(pos, s1.length(), s2);
 			ifs.close();
-			std::ofstream	ofs(av[1]);
+			std::ofstream	ofs(new_file);
 			ofs << final_str << std::endl;
 			ofs.close();
 		}
